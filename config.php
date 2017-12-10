1bcec023e811702d5ec620d4cf3748e50e2b0a7a
dirtyak <supmarvin@gmail.com>
2017-12-10 21:12:13 +0000

    Update

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$xios_name = "XIOS";  	   # Custom Name to show for each node
+$xios_dir = "/root/.XIOS"; # Directory where config files stored
+$xios_ip = localhost;      # XIOSd is running local
+$xios_port = 9000;	   # port used by first XIOSd
+$xios_count = 3;	   # How many XIOSd to monitor ?
+?>
---
+++ footer.php
@@ -0,0 +1,14 @@
+
+      </section>
+    </div>
+
+</br>
+<hr>
+<div>
+<code>
+      powered by <a href="http://www.xioscoin.com/">xios</a>, code is available on <a href="https://github.com/dirtyak/xios-monitor/">GitHub</a>
+</code>
+</div>
+
+  </body>
+</html>
