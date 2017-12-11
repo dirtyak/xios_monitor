ef8f7b154058e668d848d097605ab020c8b7f679
dirtyak <supmarvin@gmail.com>
2017-12-11 01:54:21 +0100

    Create startnode.sh

---
+++ startnode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/root/xios/src/XIOSd -datadir=/root/.XIOS1 -config=/root/.XIOS1/XIOS.conf -daemon
+#/root/xios/src/XIOSd -datadir=/root/.XIOS2 -config=/root/.XIOS2/XIOS.conf -daemon
+#/root/xios/src/XIOSd -datadir=/root/.XIOS3 -config=/root/.XIOS3/XIOS.conf -daemon
